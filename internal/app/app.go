c556d42df7468d8d77a50c17a954c2c85b75bbdd
Anton Kraev <antonkraeww@gmail.com>
2024-09-17 18:36:07 +0300

    logger setup

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,9 +1,21 @@
 package app
 
 import (
+	"fmt"
 	"log"
+	"log/slog"
+	"os"
+
+	"github.com/spf13/viper"
 
 	"github.com/Anton-Kraev/event-timeslot-planner/internal/config"
+)
+
+type envType string
+
+const (
+	envLocal envType = "local"
+	envProd  envType = "prod"
 )
 
 func Run() {
@@ -11,10 +23,34 @@ func Run() {
 		log.Fatalf("error initializing config: %s", err.Error())
 	}
 
-	// TODO: init logger: slog
+	env := viper.GetString("env")
+
+	logger, err := setupLogger(envType(env))
+	if err != nil {
+		log.Fatalf("error initializing logger: %s", err.Error())
+	}
+
+	logger.Info("starting event-timeslot-planner", slog.String("env", env))
+	logger.Debug("debug logging enabled")
+
 	// TODO: init tt cache
 	// TODO: init schedule service
 	// TODO: init schedule controller
 	// TODO: init router: chi/stdlib
 	// TODO: run server
+}
+
+func setupLogger(env envType) (*slog.Logger, error) {
+	switch env {
+	case envLocal:
+		return slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		), nil
+	case envProd:
+		return slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		), nil
+	}
+
+	return nil, fmt.Errorf("invalid env: %s", env)
 }
