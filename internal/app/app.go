8a8ee0c551c85072f5ea4536ae23b726f4f1e0bb
Anton Kraev <antonkraeww@gmail.com>
2024-09-18 21:24:13 +0300

    timetable client and schedule service init

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,12 +5,16 @@ import (
 	"fmt"
 	"log"
 	"log/slog"
+	"net/http"
 	"os"
 
 	"github.com/redis/go-redis/v9"
 	"github.com/spf13/viper"
 
+	ttcli "github.com/Anton-Kraev/event-timeslot-planner/internal/client/timetable"
 	"github.com/Anton-Kraev/event-timeslot-planner/internal/config"
+	ttrepo "github.com/Anton-Kraev/event-timeslot-planner/internal/repository/redis/timetable"
+	"github.com/Anton-Kraev/event-timeslot-planner/internal/service/schedule"
 )
 
 type envType string
@@ -35,15 +39,23 @@ func Run() {
 	logger.Info("starting event-timeslot-planner", slog.String("env", env))
 	logger.Debug("debug logging enabled")
 
-	ctx := context.Background()
+	redisConfig := viper.Sub("redis")
+	if redisConfig == nil {
+		log.Fatal("invalid config: redis is empty")
+	}
 
-	redisConfig := viper.GetStringMap("redis")
+	redisAddr := redisConfig.GetString("address")
+	if redisAddr == "" {
+		log.Fatal("invalid redis config: address is empty")
+	}
+
 	redisClient := redis.NewClient(&redis.Options{
-		Addr:     redisConfig["addr"].(string),
-		Password: redisConfig["password"].(string),
-		DB:       redisConfig["db"].(int),
+		Addr:     redisAddr,
+		Password: redisConfig.GetString("password"),
+		DB:       redisConfig.GetInt("db"),
 	})
 
+	ctx := context.Background()
 	pingRes, err := redisClient.Ping(ctx).Result()
 	if err != nil {
 		log.Fatalf("error connecting redis: %s", err.Error())
@@ -53,7 +65,33 @@ func Run() {
 
 	defer redisClient.Close()
 
-	// TODO: init schedule service
+	redisExpirationPeriod := redisConfig.GetDuration("expiration_period")
+	if redisExpirationPeriod == 0 {
+		log.Fatal("invalid redis config: expiration_period is empty")
+	}
+
+	ttCache := ttrepo.NewRedisRepository(redisClient, redisExpirationPeriod)
+
+	ttConfig := viper.Sub("timetable_api")
+	if ttConfig == nil {
+		log.Fatal("invalid config: timetable_api is empty")
+	}
+
+	ttAddress := ttConfig.GetString("address")
+	if ttAddress == "" {
+		log.Fatal("invalid timetable_api config: address is empty")
+	}
+
+	ttTimeout := ttConfig.GetDuration("timeout")
+	if ttTimeout == 0 {
+		log.Fatal("invalid timetable_api config: timeout is empty")
+	}
+
+	httpClient := &http.Client{Timeout: ttTimeout}
+	ttClient := ttcli.NewClient(ttAddress, httpClient)
+
+	_ = schedule.NewService(ttClient, ttCache)
+
 	// TODO: init schedule controller
 	// TODO: init router: chi/stdlib
 	// TODO: run server
