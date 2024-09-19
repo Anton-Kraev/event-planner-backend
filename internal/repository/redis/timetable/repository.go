c21d166204343468036cbf8a766ed0560053b01e
Anton Kraev <antonkraeww@gmail.com>
2024-09-19 21:10:44 +0300

    error wrapping in timetable redis repository

--- internal/repository/redis/timetable/repository.go
+++ internal/repository/redis/timetable/repository.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/redis/go-redis/v9"
@@ -21,27 +22,31 @@ func NewRedisRepository(client *redis.Client, expirationPeriod time.Duration) Re
 }
 
 func (c RedisRepository) GetEvents(ctx context.Context, owner timetable.CalendarOwner) ([]timetable.Event, error) {
+	const op = "repository.redis.timetable.GetEvents"
+
 	key := owner.String()
 
 	bVal, err := c.client.Get(ctx, key).Bytes()
 	if errors.Is(err, redis.Nil) {
-		return nil, timetable.ErrNotCachedYet
+		return nil, fmt.Errorf("%s: %w", op, timetable.ErrNotCachedYet)
 	}
 
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
 	var events []timetable.Event
 
 	if err = json.Unmarshal(bVal, &events); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
 	return events, nil
 }
 
 func (c RedisRepository) SetEvents(ctx context.Context, owner timetable.CalendarOwner, events []timetable.Event) error {
+	const op = "repository.redis.timetable.SetEvents"
+
 	key := owner.String()
 
 	if c.client.Exists(ctx, key).Val() != 0 {
@@ -50,8 +55,12 @@ func (c RedisRepository) SetEvents(ctx context.Context, owner timetable.Calendar
 
 	bVal, err := json.Marshal(events)
 	if err != nil {
-		return err
+		return fmt.Errorf("%s: %w", op, err)
 	}
 
-	return c.client.Set(ctx, key, bVal, c.exp).Err()
+	if err = c.client.Set(ctx, key, bVal, c.exp).Err(); err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
 }
